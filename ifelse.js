7e699c224153318c2bad9ff6c24d0fcb64f8c18b
toriazao <142436669+toriazao@users.noreply.github.com>
2024-02-27 10:59:47 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,18 @@
+// e uma variavel x, e o seu valor e 1 terminado com ; (n lembro muito, nem achei na pesquisa)
+var x = 1;
+
+//e uma variavel y, e o seu valor e 1 terminado com ;
+var y = 1;
+
+// estrutura condicional que verifica se x e igual a y
+if (x == y) {
+
+// se a mensagem do if for verdadeira, imprime o texto como uma mensagem de log "numeros conferem!", so oq lembro
+    console.log("Numeros conferem!");
+
+// permite maior controle sobre o código que o comando mais básico if, por permitir múltiplos testes serem agrupados juntos
+} else {
+
+// ja coloquei tbm
+    console.log("Numeros divergem!");
+}
